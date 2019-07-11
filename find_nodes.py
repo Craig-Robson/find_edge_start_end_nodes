6d923c374a6368768ad1ca45200cd130b4a70cde
Craig Robson <craig.robson1@ncl.ac.uk>
2019-07-11 20:35:08 +0100

    updated to run in multicore

--- find_nodes.py
+++ find_nodes.py
@@ -8,11 +8,55 @@ def database_connection(db_params):
     """
 
     connection = psycopg2.connect("dbname=%s user=%s port=%s host=%s password=%s" %(db_params['database_name'], db_params['user'], db_params['port'], db_params['host'], db_params['password']))
-
+    connection.autocommit = True
     return connection
 
 
-def main(edges, nodes, edge_id_field='gid', node_id_field='gid', connection=None, connection_parameters=None, update_edges=False):
+# def edge_analysis(node_id_field, egde_id_field, nodes, node_geom_field, feat, edges, db_params):
+def edge_analysis(feat_):
+    feat = feat_[0]
+    params = feat_[1]
+
+    node_id_field = params['node_id_field']
+    edge_id_field = params['edge_id_field']
+    nodes = params['nodes']
+    node_geom_field = params['node_geom_field']
+    edges = params['edges']
+    db_params = params['connection_parameters']
+    update_edges = params['update_edges']
+
+    conn = database_connection(db_params)
+    cursor = conn.cursor()
+
+    print(feat)
+    # run for start node
+    cursor.execute(sql.SQL('SELECT {} FROM {} ORDER BY ST_Transform({}, 27700) <-> %s LIMIT 1;').format(sql.Identifier(
+        node_id_field), sql.Identifier(nodes), sql.Identifier(node_geom_field)), [feat[1]])
+
+    start_node_id = cursor.fetchone()[0]
+
+    # run for end node
+    cursor.execute(sql.SQL('SELECT {} FROM {} ORDER BY ST_Transform({}, 27700) <-> %s LIMIT 1;').format(sql.Identifier(
+        node_id_field), sql.Identifier(nodes), sql.Identifier(node_geom_field)), [feat[2]])
+
+    end_node_id = cursor.fetchone()[0]
+
+    # update the edges with the to and from ids if set as True
+    if update_edges:
+        cursor.execute(sql.SQL('UPDATE {} SET from_id=%s, to_id=%s WHERE gid=%s;').format(sql.Identifier(
+            edges)), [start_node_id, end_node_id, feat[0]])
+
+    # store the information for use when inserting data in to the graph database
+    # edge_set[feat[0]] = {'start_node':start_node_id, 'end_node':end_node_id}
+    temp = {'start_node': start_node_id, 'end_node': end_node_id}
+
+    cursor.close()
+    conn.close()
+    return temp
+
+
+def main(edges, nodes, edge_id_field='gid', node_id_field='gid', connection=None, connection_parameters=None,
+         update_edges=False):
     """
     Calculate the closest node in a node dataset to the start and end of each edge in an edge dataset.
     """
@@ -38,8 +82,11 @@ def main(edges, nodes, edge_id_field='gid', node_id_field='gid', connection=None
 
     edge_geom_field = cursor.fetchone()[0]
 
-    # get edges from database
-    cursor.execute(sql.SQL('SELECT {}, ST_AsEWKT(ST_StartPoint(ST_SetSRID({},27700))) as start_node, ST_AsEWKT(ST_EndPoint(ST_SetSRID({}, 27700))) as end_node FROM {}').format(sql.Identifier(edge_id_field), sql.Identifier(edge_geom_field), sql.Identifier(edge_geom_field), sql.Identifier(edges)))
+    # get edges from database - grab 50 at a time
+    cursor.execute(sql.SQL(
+        'SELECT {}, ST_AsEWKT(ST_Force_2D(ST_StartPoint(ST_SetSRID({},27700)))) as start_node, ST_AsEWKT(ST_Force_2D(ST_EndPoint(ST_SetSRID({}, 27700)))) as end_node FROM {} WHERE to_id is null LIMIT 10').format(
+        sql.Identifier(edge_id_field), sql.Identifier(edge_geom_field), sql.Identifier(edge_geom_field),
+        sql.Identifier(edges)))
 
     edge_features = cursor.fetchall()
 
@@ -51,24 +98,21 @@ def main(edges, nodes, edge_id_field='gid', node_id_field='gid', connection=None
     node_geom_field = cursor.fetchone()[0]
 
     # loop through the edges returned from the database
+    temp_ = {'node_id_field': node_id_field,
+             'edge_id_field': edge_id_field,
+             'nodes': nodes,
+             'node_geom_field': node_geom_field,
+             'edges': edges,
+             'connection_parameters': connection_parameters,
+             'update_edges': update_edges}
+
+    feat_ = []
     for feat in edge_features:
+        feat_.append([feat, temp_])
 
-        # run for start node
-        cursor.execute(sql.SQL('SELECT {} FROM {} ORDER BY ST_Transform({}, 27700) <-> %s LIMIT 1;').format(sql.Identifier(node_id_field), sql.Identifier(nodes), sql.Identifier(node_geom_field)), [feat[1]])
+    with mp.Pool(5) as p:
 
-        start_node_id = cursor.fetchone()[0]
-
-        # run for end node
-        cursor.execute(sql.SQL('SELECT {} FROM {} ORDER BY ST_Transform({}, 27700) <-> %s LIMIT 1;').format(sql.Identifier(node_id_field), sql.Identifier(nodes), sql.Identifier(node_geom_field)), [feat[2]])
-
-        end_node_id = cursor.fetchone()[0]
-
-        # update the edges with the to and from ids if set as True
-        if update_edges:
-
-            cursor.execute(sql.SQL('UPDATE {} SET from_id=%s, to_id=%s WHERE gid=%s;').format(sql.Identifier(edges)), [start_node_id, end_node_id, feat[edge_id_field]])
-
-        # store the information for use when inserting data in to the graph database
-        edge_set[feat[0]] = {'start_node':start_node_id, 'end_node':end_node_id}
+        print(p.map(edge_analysis, feat_))
+        # (node_id_field, egde_id_field, nodes, node_geom_field, feat, edges, connection_parameters)
 
     return edge_set
